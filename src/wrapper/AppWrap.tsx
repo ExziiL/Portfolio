7ccd48cdf18409ab9d9de452b1bd49949fd9d2e5
Andre Derjagin <derjagin.andre@gmail.com>
2022-08-26 18:49:40 +0200

    finished video

--- src/wrapper/AppWrap.tsx
+++ src/wrapper/AppWrap.tsx
@@ -15,10 +15,10 @@ const AppWrap = (Component: any, idName: string | undefined, classNames?: any) =
 				<div className="app__wrapper app__flex">
 					<Component />
 
-					<div className="copyright">
+					{/* <div className="copyright">
 						<p className="p-text">@2022 Andre Derjagin</p>
 						<p className="p-text">All rights reserved</p>
-					</div>
+					</div> */}
 				</div>
 				<NavigationDots active={idName} />
 			</div>
